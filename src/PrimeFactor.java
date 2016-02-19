bee36a1f7a023ee7f90adaffb228ccd7b1d140be
Lucy Fang <lucyfang2@gmail.com>
2016-02-19 15:21:48 +0000

    can print out prime factor numbers

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -6,28 +6,40 @@ import java.util.*;
 public class PrimeFactor {
   int number;
   ArrayList<Integer> factors = new ArrayList<>();
+  ArrayList<Integer> primes = new ArrayList<>();
 
 
   PrimeFactor(int n) {
     number = n;
   }
 
+  boolean isPrime(int n) {
+    if (n == 2) primes.add(n);
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) return false;
+    }
+    primes.add(n);
+    return true;
+  }
+
   public ArrayList<Integer> findFactors() {
-    if (number == 1) {
-      factors.add(0);
-    }
+    if (number == 1) return primes;
     for(int i = 2; i <= number; i++) {
-      if (number % i == 0) {
-        factors.add(i);
-      }
+      if (number % i == 0) factors.add(i);
     }
-    for (int e: factors) {
+    for (int e : factors) {
+      isPrime(e);
+    }
+    for (int e : primes) {
       System.out.println(e);
     }
-    return factors;
+    return primes;
   }
 
   public static void main(String[] args) {
-    new PrimeFactor(30).findFactors();
+    PrimeFactor primeTest = new PrimeFactor(30);
+    primeTest.findFactors();
+    new PrimeFactor(1);
   }
 }
